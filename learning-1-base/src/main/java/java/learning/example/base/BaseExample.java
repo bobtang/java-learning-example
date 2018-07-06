826f697b69219c3072022e728c94997276e33d3a
tangyangbo <tangyangbo@yaowan.com>
2018-07-06 16:24:37 +0800

    add base example

---
+++ learning-1-base/src/main/java/java/learning/example/base/BaseExample.java
@@ -0,0 +1,11 @@
+package java.learning.example.base;
+
+/**
+ * java 基础学习示例
+ */
+public class BaseExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
