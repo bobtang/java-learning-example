74d339466ecb3dac54b548f036ef45373dc14f73
mobingsen <2740303622@qq.com>
2018-08-01 16:38:16 +0800

    jdk动态代理(接口)

--- learning-5-dynamicproxy/src/main/java/learning/dynamicproxy/DogX.java
+++ learning-5-dynamicproxy/src/main/java/learning/dynamicproxy/DogX.java
@@ -9,14 +9,19 @@ import java.lang.reflect.Method;
  * @Author mbs
  * @Date 2018/7/30 15:57
  */
+// 被代理对象调用处理程序，必须实现InvocationHandler接口
 public class DogX implements InvocationHandler {
-
+    // 所代理的真实对象
     private WorkInteface target;
 
     public DogX(WorkInteface target) {
         this.target = target;
     }
 
+    //需要我们实现具体业务的地方
+    //proxy:　　所生成代理类实例
+    //method:　　指代的是我们所要调用真实对象的某个方法的Method对象
+    //args:　　指代的是调用真实对象某个方法时接受的参数
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("dog start...");
