27f3fdfc70bb19a824988db5ff97456e1548f1f0
zhongyuanxing <zhongyuanxing@yaowan.com>
2018-09-06 11:14:35 +0800

    learning-8

---
+++ learning-8-voliate/src/main/java/main/VolatileTestA.java
@@ -0,0 +1,96 @@
+package main;
+
+/**
+ * 
+ *  volatile防止重排序->内存屏障
+ * 
+ *  
+ *  很容易想到这段代码的运行结果可能为(1,0)、(0,1)或(1,1)，因为线程one可以在线程two开始之前就执行完了，
+ *  也有可能反之，甚至有可能二者的指令是同时或交替执行的。
+ * 
+ *  然而，这段代码的执行结果也可能是(0,0).(发现一次跑太久不出来，关掉重新编译)
+ *
+ */
+public class VolatileTestA {
+	
+	static int i;
+	
+	int x = 0, y = 0;
+	int a = 0, b = 0;
+
+//	volatile int x = 0;
+//	volatile int y = 0;
+//	volatile int a = 0;
+//  volatile int b = 0;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		while (true) {
+			VolatileTestA p = new VolatileTestA();
+			Thread one = new Thread(new Runnable() {
+				public void run() {
+					p.a = 1;
+					p.x = p.b;
+				}
+			}, "A");
+
+			Thread other = new Thread(new Runnable() {
+				public void run() {
+					p.b = 1;
+					p.y = p.a;
+				}
+			}, "B");
+			one.start();
+			other.start();
+			one.join();
+			other.join();
+			
+			System.out.println(String.format("第{%s}次，(%s,%s) ", i++, p.x, p.y));
+			
+			if (p.x == 0 && p.y == 0) {
+				System.out.println("exit (0,0)");
+				break;
+			}
+		}
+	}
+	
+	// 一个在项目里面见过的bug，判断某个bean里面的boolean是否为ture，若是true执行逻辑。
+	class WrongExample {
+
+	    int i = 0;
+	    volatile boolean flag = false;
+
+	    //Thread A
+	    public void write(){
+	        i = 2;              //1
+	        flag = true;        //2
+	    }
+
+	    //Thread B
+	    public void read(){
+	        if(flag){                                   //3
+	            System.out.println("---i = " + i);      //4
+	        }
+	    }
+	    
+	    public void main(String[] args) throws InterruptedException {
+	    	WrongExample wrongExample = new WrongExample();
+	    	Thread one = new Thread(new Runnable() {
+				public void run() {
+					wrongExample.write();
+				}
+			}, "A");
+
+			Thread other = new Thread(new Runnable() {
+				public void run() {
+					wrongExample.read();
+				}
+			}, "B");
+			one.start();
+			other.start();
+			one.join();
+			other.join();
+	    }
+	    	
+	}
+}
---
+++ learning-8-voliate/src/main/java/main/VolatileTestB.java
@@ -0,0 +1,36 @@
+package main;
+
+/**
+ * 
+ * volatile原子性：对任意单个volatile变量的读/写具有原子性，但类似于volatile++这种复合操作不具有原子性
+ * 
+ */
+public class VolatileTestB {
+	volatile int i;
+
+	public void addI() {
+		i++;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		final VolatileTestB testB = new VolatileTestB();
+		for (int n = 0; n < 1000; n++) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					testB.addI();
+				}
+			}).start();
+		}
+
+		Thread.sleep(10000);//等待10秒，保证上面程序执行完成
+
+		System.out.println(testB.i);
+	}
+
+}
