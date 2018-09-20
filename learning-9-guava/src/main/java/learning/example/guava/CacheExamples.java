a707d3132e963d9f8b8f8a74572ffe84d7b81ff7
LeiJun <leijun@yaowan.com>
2018-09-20 18:46:12 +0800

    调整

--- learning-9-guava/src/main/java/learning/example/guava/CacheExamples.java
+++ learning-9-guava/src/main/java/learning/example/guava/CacheExamples.java
@@ -1,14 +1,12 @@
 package learning.example.guava;
 
 import com.google.common.base.Function;
-import com.google.common.base.Throwables;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.cache.RemovalListener;
 import org.junit.Test;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,19 +14,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class CacheExamples {
 
-    private final LoadingCache<Integer, String> cache = CacheBuilder.newBuilder()
-            .expireAfterAccess(5, TimeUnit.SECONDS)
-//            .expireAfterWrite(5, TimeUnit.SECONDS)
-//            .maximumSize(1)
-//            .removalListener(this.removalListener)
-            .build(new CacheLoader<Integer, String>() {
-                @Override
-                public String load(Integer key) throws Exception {
-                    return loader.apply(key);
-                }
-            });
-
-    private final Function<Integer, String> loader = key -> {
+    private final Function<Integer, String> creator = key -> {
         String value = "v" + key;
         System.out.println("create value: " + value);
         return value;
@@ -38,20 +24,30 @@ public class CacheExamples {
         System.out.println(String.format("k = %s remove, cause %s", notification.getKey(), notification.getCause()));
     };
 
-    private String get(int key) {
-        try {
-            return cache.get(key);
-        } catch (ExecutionException e) {
-            Throwables.throwIfUnchecked(e);
-            throw new RuntimeException(e);
-        }
-    }
+    private final LoadingCache<Integer, String> cache = CacheBuilder.newBuilder()
+            .expireAfterAccess(1, TimeUnit.SECONDS)
+//            .expireAfterWrite(1, TimeUnit.SECONDS)
+            .maximumSize(10)
+            .removalListener(removalListener)
+            .build(new CacheLoader<Integer, String>() {
+                @Override
+                public String load(Integer key) throws Exception {
+                    return creator.apply(key);
+                }
+            });
+
+    // 使用CacheBuilder构建的缓存不会"自动"执行清理和回收工作，
+    // 也不会在某个缓存项过期后马上清理，也没有诸如此类的清理机制。
+    // 相反，它会在写操作时顺带做少量的维护工作，或者偶尔在读操作时做——如果写操作实在太少的话。
 
     @Test
     public void testLoadingCache() throws InterruptedException {
-        System.out.println(get(1));
-        System.out.println(get(2));
+        System.out.println(cache.getUnchecked(1));
+        System.out.println(cache.getUnchecked(2));
         System.out.println(cache.size());
-        Thread.sleep(10 * 1000);
+        Thread.sleep(5 * 1000);
+        cache.getUnchecked(3);
+        Thread.sleep(1 * 1000);
+        System.out.println(cache.size());
     }
 }
